1d87994e832a3f0c52d152df94a87312bb5d1539
Parsa Mohammadian <parsa2820@gmail.com>
2022-09-21 23:35:18 +0430

    Change some of the emacs configs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -19,32 +19,44 @@
 ;;              '("melpa" . "https://melpa.org/packages/"))
 ;; (package-initialize)
 ;; (package-refresh-contents)
-;; 
+
 ;; ;; Download evil
 ;; (unless (package-installed-p 'evil)
 ;;   (package-install 'evil))
-;; 
+
 ;; ;; Download ivy
 ;; (unless (package-installed-p `ivy)
 ;;   (package-install `ivy))
-;; 
+
 ;; ;; Download org-bullets
 ;; (unless (package-installed-p `org-bullets)
 ;;   (package-install `org-bullets))
-;; 
+
 ;; ;; Download rainbow-delimiters
 ;; (unless (package-installed-p `rainbow-delimiters)
 ;;   (package-install `rainbow-delimiters))
-;; 
+
 ;; ;; Download org-tree-slide
 ;; (unless (package-installed-p `org-tree-slide)
 ;;   (package-install `org-tree-slide))
-;; 
+
 ;; ;; Download dockerfile-mode
 ;; (unless (package-installed-p `dockerfile-mode)
 ;;   (package-install `dockerfile-mode))
 
+;; ;; Download which-key
+;; (unless (package-installed-p `which-key)
+;;   (package-install `which-key))
+
+;; ;; Download undo-tree for undo functionality in evil-mode
+;; (unless (package-installed-p `undo-tree)
+;;   (package-install `undo-tree))
+
 ;; Enable Evil
+;; Disable tab key functionality in evil mode
+;; In order to use tab key for org-cycle command
+(setq evil-want-C-i-jump nil)
+(setq evil-want-fine-undo t)
 (require 'evil)
 (evil-mode 1)
 (custom-set-variables
@@ -57,15 +69,21 @@
  '(ansi-color-names-vector
    ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
  '(custom-enabled-themes nil)
+ '(global-display-line-numbers-mode t)
  '(ispell-dictionary nil)
  '(package-selected-packages
-   '(dockerfile-mode org-tree-slide rainbow-delimiters evil cmake-mode)))
+   '(undo-tree which-key dockerfile-mode org-tree-slide rainbow-delimiters evil cmake-mode))
+ '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:family "Source Code Pro" :foundry "ADBO" :slant normal :weight normal :height 158 :width normal)))))
+
+;; Enable undo/redo for the evil mode
+(global-undo-tree-mode)
+(evil-set-undo-system 'undo-tree)
 
 ;; Enable Ivy
 (ivy-mode)
@@ -76,22 +94,22 @@
 (load-theme 'misterioso)
 
 ;; Org agenda
-(setq org-agenda-files '("~/org/"))
+(setq org-agenda-files '("~/Dropbox/org/"))
 
 ;; Config org-bullets
 (require 'org-bullets)
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 
-;; Config org titles font size
-(dolist (face '((org-level-1 . 1.2)
-		(org-level-2 . 1.1)
-                (org-level-3 . 1.05)
-                (org-level-4 . 1.0)
-                (org-level-5 . 1.1)
-                (org-level-6 . 1.1)
-                (org-level-7 . 1.1)
-                (org-level-8 . 1.1)))
-  (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
+;; ;; Config org titles font size
+;; (dolist (face '((org-level-1 . 1.2)
+;; 		(org-level-2 . 1.1)
+;;                 (org-level-3 . 1.05)
+;;                 (org-level-4 . 1.0)
+;;                 (org-level-5 . 1.1)
+;;                 (org-level-6 . 1.1)
+;;                 (org-level-7 . 1.1)
+;;                 (org-level-8 . 1.1)))
+;;   (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
 
 ;; Config org inline image size
 (setq org-image-actual-width nil)
@@ -117,4 +135,9 @@
     (shell . t)
     (python . t)))
 
-(text-scale-set 0)
+;; Enable which-key
+(which-key-mode 1)
+
+;; Configure dired
+;; Do not show hidden dot files
+(setq dired-listing-switches "-lh")
